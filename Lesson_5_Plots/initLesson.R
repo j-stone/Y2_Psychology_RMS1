0a20088b4cd79d61a7ad34a6f3c284b83fd83c87
Josiah King <josiahpjking@gmail.com>
2015-10-13 15:44:45 +0100

    lesson 5 and 6 as per Ellis errors

--- Lesson_5_Plots/initLesson.R
+++ Lesson_5_Plots/initLesson.R
@@ -2,4 +2,4 @@
 # lesson is started. Any variables created here will show up in
 # the user's working directory and thus be accessible to them
 # throughout the lesson.
-detach("package:openintro", unload=TRUE)
+
